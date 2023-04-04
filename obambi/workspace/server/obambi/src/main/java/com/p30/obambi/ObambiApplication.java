a9a98419967b3ec7c5da53812a715f9ba43e692c
park sang young <wildbear94@gmail.com>
2023-04-04 16:52:02 +0900

    기초소스및 문서 등록

---
+++ obambi/workspace/server/obambi/src/main/java/com/p30/obambi/ObambiApplication.java
@@ -0,0 +1,13 @@
+package com.p30.obambi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObambiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObambiApplication.class, args);
+    }
+
+}
---
+++ obambi/workspace/server/obambi/src/test/java/com/p30/obambi/ObambiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p30.obambi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObambiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
